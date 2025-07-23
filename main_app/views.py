67986f7fbe3483fd1c5b4c01b8ac9429845d5990
timothyn <tnguyen_42@yahoo.com>
2025-07-23 16:05:24 -0400

    Set up Views

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,62 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import ListView, DetailView, CreateView, TemplateView
+from .forms import SignUpForm, LoginForm, SkillForm
+from .models import Skill
 
-# Create your views here.
+class HomeView(TemplateView):
+    template_name = 'home.html'
+
+def signup_view(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('my_skills')
+    else:
+        form = UserCreationForm()
+    return render(request, 'signup.html', {'form': form})
+
+def login_view(request):
+    form = LoginForm(request.POST or None)
+    if request.method == 'POST' and form.is_valid():
+        user = authenticate(
+            request,
+            username=form.cleaned_data['username'],
+            password=form.cleaned_data['password']
+        )
+        if user:
+            login(request, user)
+            return redirect('my_skills')
+        form.add_error(None, 'Invalid Username or Password.')
+    return render(request, 'login.html', {'form': form})
+
+def logout_view(request):
+    logout(request)
+    return redirect('landing')
+
+class SkillListView(LoginRequiredMixin, ListView):
+    model = Skill
+    template_name = 'skills/list.html'
+    context_object_name = 'skills'
+
+    def get_queryset(self):
+        return Skill.objects.filter(user=self.request.user)
+
+class SkillDetailView(LoginRequiredMixin, DetailView):
+    model = Skill
+    template_name = 'skills/detail.html'
+
+
+class SkillCreateView(LoginRequiredMixin, CreateView):
+    model = Skill
+    form_class = SkillForm
+    template_name = 'skills/form.html'
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
