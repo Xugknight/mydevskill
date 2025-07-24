7740488c35a62b5001eff873bd78b06469d06c7f
timothyn <tnguyen_42@yahoo.com>
2025-07-24 15:26:55 -0400

    Fix field labeling, custom style for sign up page. Enforce consistent dark mode for all fields

--- main_app/forms.py
+++ main_app/forms.py
@@ -1,14 +1,37 @@
 from django import forms
-from django.forms import Form, CharField, PasswordInput
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from .models import Skill
 
-class LoginForm(Form):
-    username = CharField(label='User Name', max_length=64)
-    password = CharField(widget=PasswordInput())
+class LoginForm(forms.Form):
+    username = forms.CharField(label='Username', max_length=64)
+    password = forms.CharField(widget=forms.PasswordInput())
 
 class SignUpForm(UserCreationForm):
+    username = forms.CharField(
+        max_length=64,
+        widget=forms.TextInput(attrs={
+            'placeholder': 'Enter a username',
+            'class': 'input-field',
+        })
+    )
+
+    password1 = forms.CharField(
+        label='Password',
+        widget=forms.PasswordInput(attrs={
+            'placeholder': 'Enter password',
+            'class': 'input-field',
+        })
+    )
+
+    password2 = forms.CharField(
+        label='Confirm Password',
+        widget=forms.PasswordInput(attrs={
+            'placeholder': 'Repeat password',
+            'class': 'input-field',
+        })
+    )
+
     class Meta:
         model = User
         fields = ['username', 'password1', 'password2']
@@ -21,7 +44,8 @@ class SkillForm(forms.ModelForm):
         widgets = {
             'description': forms.Textarea(attrs={
                 'rows': 2, 
-                'placeholder': 'Describe your skill'
+                'placeholder': 'Describe your skill',
+                'class': 'input-field',
                 }),
-                'skill_level': forms.Select(),
+                'skill_level': forms.Select(attrs={'class': 'input-field'}),
         }
--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth import authenticate, login, logout
-from django.contrib.auth.forms import UserCreationForm
+# from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import ListView, DetailView, CreateView, TemplateView
@@ -12,13 +12,13 @@ class HomeView(TemplateView):
 
 def signup_view(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = SignUpForm(request.POST)
         if form.is_valid():
             user = form.save()
             login(request, user)
             return redirect('my_skills')
     else:
-        form = UserCreationForm()
+        form = SignUpForm()
     return render(request, 'signup.html', {'form': form})
 
 def login_view(request):
